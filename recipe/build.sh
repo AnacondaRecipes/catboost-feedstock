0d035618279736e8b9369a11396c9210eb3a81f0
Kai Tietz <ktietz@anaconda.com>
2022-08-01 10:39:49 +0200

    adjust linux archs

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,6 +20,7 @@ if [ `uname` == Darwin ]; then
     fi
 fi
 
+echo "ARCH: $ARCH ..."
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "2.7" ]; then
         WHL_FILE=https://pypi.org/packages/cp27/c/catboost/catboost-${PKG_VERSION}-cp27-none-manylinux1_x86_64.whl
@@ -28,13 +29,13 @@ if [ `uname` == Linux ]; then
     elif [ "$PY_VER" == "3.6" ]; then
         WHL_FILE=https://pypi.org/packages/cp36/c/catboost/catboost-${PKG_VERSION}-cp36-none-manylinux1_x86_64.whl
     elif [ "$PY_VER" == "3.7" ]; then
-        WHL_FILE=https://pypi.org/packages/cp37/c/catboost/catboost-${PKG_VERSION}-cp37-none-manylinux1_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp37/c/catboost/catboost-${PKG_VERSION}-cp37-none-manylinux1_${ARCH}.whl
     elif [ "$PY_VER" == "3.8" ]; then
-        WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-none-manylinux1_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-none-manylinux1_${ARCH}.whl
     elif [ "$PY_VER" == "3.9" ]; then
-        WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-none-manylinux1_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-none-manylinux1_${ARCH}.whl
     elif [ "$PY_VER" == "3.10" ]; then
-        WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-none-manylinux1_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-none-manylinux1_${ARCH}.whl
     fi
 fi
 
