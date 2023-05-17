86f236c3b9d9ae458615f786ad013ad55cec0aac
Serhii Kupriienko <skupriienko@anaconda.com>
2023-05-17 12:02:07 +0300

    Install from the source code

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,15 +5,16 @@ set -xe
 # install using pip from the whl files on PyPI
 
 if [ `uname` == Darwin ]; then
-    if [ "$PY_VER" == "3.8" ]; then
-        WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-cp38-macosx_11_0_universal2.whl
-    elif [ "$PY_VER" == "3.9" ]; then
-        WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-cp39-macosx_11_0_universal2.whl
-    elif [ "$PY_VER" == "3.10" ]; then
-        WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-cp310-macosx_11_0_universal2.whl
-    elif [ "$PY_VER" == "3.11" ]; then
-        WHL_FILE=https://pypi.org/packages/cp311/c/catboost/catboost-${PKG_VERSION}-cp311-cp311-macosx_11_0_universal2.whl
-    fi
+    $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed -vv
+    # if [ "$PY_VER" == "3.8" ]; then
+    #     WHL_FILE=https://pypi.org/packages/cp38/c/catboost/catboost-${PKG_VERSION}-cp38-cp38-macosx_11_0_universal2.whl
+    # elif [ "$PY_VER" == "3.9" ]; then
+    #     WHL_FILE=https://pypi.org/packages/cp39/c/catboost/catboost-${PKG_VERSION}-cp39-cp39-macosx_11_0_universal2.whl
+    # elif [ "$PY_VER" == "3.10" ]; then
+    #     WHL_FILE=https://pypi.org/packages/cp310/c/catboost/catboost-${PKG_VERSION}-cp310-cp310-macosx_11_0_universal2.whl
+    # elif [ "$PY_VER" == "3.11" ]; then
+    #     WHL_FILE=https://pypi.org/packages/cp311/c/catboost/catboost-${PKG_VERSION}-cp311-cp311-macosx_11_0_universal2.whl
+    # fi
 fi
 
 echo "ARCH: $ARCH ..."
@@ -34,6 +35,6 @@ if [ `uname` == Linux ]; then
     elif [ "$PY_VER" == "3.11" ]; then
         WHL_FILE=https://pypi.org/packages/cp311/c/catboost/catboost-${PKG_VERSION}-cp311-cp311-manylinux2014_${TARGET_ARCH}.whl
     fi
-fi
 
-$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed -vv $WHL_FILE
+    $PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed -vv $WHL_FILE
+fi
